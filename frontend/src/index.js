de24560bcc776c351830ccf6150be4566e9c5f64
Nathalia Lin <32189825+nathalia-lin@users.noreply.github.com>
2019-03-05 13:22:17 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+// import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
