a66b8346e37a30ce3bc3332c3406d19df7e6f27e
Nathalia Lin <nl1532@nyu.edu>
2019-03-05 13:10:57 -0500

    add basic react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world, please!</h1>
-      </div>
+
+    // Navbar Bootstrap
+	<Navbar bg="light" expand="lg">
+	  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+	  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+	  <Navbar.Collapse id="basic-navbar-nav">
+	    <Nav className="mr-auto">
+	      <Nav.Link href="#home">Home</Nav.Link>
+	      <Nav.Link href="#link">Link</Nav.Link>
+	      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+	        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+	        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+	        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+	        <NavDropdown.Divider />
+	        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+	      </NavDropdown>
+	    </Nav>
+	    <a> Hi </a>
+	    <Form inline>
+	      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+	      <Button variant="outline-success">Search</Button>
+	    </Form>
+	  </Navbar.Collapse>
+	</Navbar>
     );
   }
 }
