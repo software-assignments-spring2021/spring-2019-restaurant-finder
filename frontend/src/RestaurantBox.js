c3cb280795380068715fade0e87b423e1e698365
Will Powers <willcpo@gmail.com>
2019-03-07 04:32:55 -0500

    added more react bbootstap functionality and search results

---
+++ frontend/src/RestaurantBox.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Row, Col, Card} from 'react-bootstrap';
+
+class RestaurantBox extends Component {
+  render() {
+    return (
+
+			<Card>
+
+				<div className="card-header">
+					<div className= "h3 class-title">{this.props.name}</div>
+				</div>
+				<Row>
+					<div className="col-6 col-md-4 col-lg-3">
+							<img style={{height: 150 + 'px'}} className="rounded" src={this.props.imgURL}/>
+					</div>
+					<Col>
+						<a className="card-link" href={this.props.webURL}>Website</a>
+						<div className="card-text">Hours of Operation: {this.props.hours}</div>
+						<div className="card-text">Address: {this.props.address}</div>
+						<div className="card-text">Phone: {this.props.phone}</div>
+						<div className="card-text">Price: {this.props.price}</div>
+						<div className="card-text">Rating: {this.props.rating}</div>
+					</Col>
+				</Row>
+				
+			</Card>
+
+	);
+  }
+}
+export default RestaurantBox;
