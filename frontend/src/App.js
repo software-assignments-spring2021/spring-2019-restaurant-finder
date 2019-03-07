f00f3010cc3738ec89aef0757979d031bbd617ef
Will Powers <willcpo@gmail.com>
2019-03-07 06:50:48 -0500

    Changed Site Title in Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
     return (
 	<>
 	<Navbar bg="light" expand="lg">
-	  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+	  <Navbar.Brand href="#home">Restaurant Finder</Navbar.Brand>
 	  <Navbar.Toggle aria-controls="basic-navbar-nav" />
 	  <Navbar.Collapse id="basic-navbar-nav">
 	    <Nav className="mr-auto">
