6939d2ec1a9853bf24f3bc13e607045d1faf11d6
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-03-25 09:38:38 -0400

    adding * route

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
