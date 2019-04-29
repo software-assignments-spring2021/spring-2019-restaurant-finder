1f12c084817398da63d129718c5b4486e0066fda
Roy Lee <hkl288@nyu.edu>
2019-04-28 22:53:43 -0400

    Restructured images inside components

--- frontend/src/components/RestaurantBox.js
+++ frontend/src/components/RestaurantBox.js
@@ -17,38 +17,40 @@ class RestaurantBox extends Component {
 		this.Auth.newFavorite(newFav);
 	}
 
+	
 	handleRatings(){
 		if (this.props.rating == 0){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_0.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_0.png')}/>
 		} 
 		if (this.props.rating == 1){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
 		}
 		if (this.props.rating == 1.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
 		}
 		if (this.props.rating == 2){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_2.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_2.png')}/>
 		}
 		if (this.props.rating == 2.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_2_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_2_half.png')}/>
 		}
 		if (this.props.rating == 3){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_3.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_3.png')}/>
 		}
 		if (this.props.rating == 3.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_3_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_3_half.png')}/>
 		}
 		if (this.props.rating == 4){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_4.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_4.png')}/>
 		}
 		if (this.props.rating == 4.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_4_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_4_half.png')}/>
 		}
 		if (this.props.rating == 5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_5.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_5.png')}/>
 		}
 	}
+	
 
 
 
@@ -70,7 +72,7 @@ class RestaurantBox extends Component {
 						<div className="card-text">Rating: {this.handleRatings()}</div>
 						<div className="card-text">Distance: {Math.round(this.props.distance)} meters</div>
 						<a href="http://yelp.com"> 
-							<img src={require('./Yelp_trademark_RGB_outline.png')} width="80" height="50" style={{position: "absolute", bottom: 0, right: 0}}/>
+							<img src={require('../images/Yelp_trademark_RGB_outline.png')} width="80" height="50" style={{position: "absolute", bottom: 0, right: 0}}/>
 						</a>
 						<br />
 					</Col>		
