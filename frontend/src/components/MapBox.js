dfc3cb4f45691d887bcb1c0b6a2639515d2966bf
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-04-28 20:53:08 -0400

    adding popups and ability to add more in future

--- frontend/src/components/MapBox.js
+++ frontend/src/components/MapBox.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Container} from 'react-bootstrap';
-import ReactMapGL from 'react-map-gl';
-
+import ReactMapGL, {Popup} from 'react-map-gl';
 let token = 'pk.eyJ1IjoiY21yNjI0IiwiYSI6ImNqdXp2YXhjNjBrZms0ZHBoejB0MjYxencifQ.RwRI3gBw5GiU-J8a3-xSlg';
 
 class MapBox extends Component {
@@ -16,17 +15,39 @@ class MapBox extends Component {
                 latitude: this.props.lat,
                 longitude: this.props.lng,
                 zoom: 15
-              }
+              },
+            locations:[
+                {
+                    lat: this.props.lat, 
+                    lng: this.props.lng,
+                    name: 'User Location!'
+                }
+            ]
+            
         };
     }
 
     render() {
         return (
-            <ReactMapGL
-            mapboxApiAccessToken = {token}
-            {...this.state.viewport}
-            onViewportChange={(viewport) => this.setState({viewport})}
-          />
+            <Container>
+                <ReactMapGL
+                    mapboxApiAccessToken = {token}
+                    {...this.state.viewport}
+                    onViewportChange={(viewport) => this.setState({viewport})}
+                >
+                {this.state.locations.map((loc) => { return (
+                <Popup 
+                    latitude={loc.lat} 
+                    longitude={loc.lng} 
+                    closeButton={false} 
+                    closeOnClick={true} 
+                    anchor="bottom"
+                    >
+                    <div>{loc.name}</div>
+                </Popup>);
+                })}
+            </ReactMapGL>
+          </Container>
         );
     }
 }
