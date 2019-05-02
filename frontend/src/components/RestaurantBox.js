0adaf58901a9579b3923f669bc3a5b455703cc9e
Roy Lee <hkl288@nyu.edu>
2019-05-01 20:40:33 -0400

    Added user rating functionality

--- frontend/src/components/RestaurantBox.js
+++ frontend/src/components/RestaurantBox.js
@@ -10,8 +10,8 @@ class RestaurantBox extends Component {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.Auth = new Auth();
-	} 
-	
+	}
+
 	handleSubmit() {
 		const newFav = {name: this.props.name, url: this.props.url};
 		this.Auth.newFavorite(newFav);
@@ -19,34 +19,34 @@ class RestaurantBox extends Component {
 
 	handleRatings(){
 		if (this.props.rating == 0){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_0.png')}/>
-		} 
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_0.png')}/>
+		}
 		if (this.props.rating == 1){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
 		}
 		if (this.props.rating == 1.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_1_half.png')}/>
 		}
 		if (this.props.rating == 2){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_2.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_2.png')}/>
 		}
 		if (this.props.rating == 2.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_2_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_2_half.png')}/>
 		}
 		if (this.props.rating == 3){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_3.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_3.png')}/>
 		}
 		if (this.props.rating == 3.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_3_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_3_half.png')}/>
 		}
 		if (this.props.rating == 4){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_4.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_4.png')}/>
 		}
 		if (this.props.rating == 4.5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_4_half.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_4_half.png')}/>
 		}
 		if (this.props.rating == 5){
-			return <img src={require('./yelp_stars/web_and_ios/regular/regular_5.png')}/>
+			return <img src={require('../images/yelp_stars/web_and_ios/regular/regular_5.png')}/>
 		}
 	}
 
@@ -69,11 +69,11 @@ class RestaurantBox extends Component {
 						<div className="card-text">Price: {this.props.price}</div>
 						<div className="card-text">Rating: {this.handleRatings()}</div>
 						<div className="card-text">Distance: {Math.round(this.props.distance)} meters</div>
-						<a href="http://yelp.com"> 
-							<img src={require('./Yelp_trademark_RGB_outline.png')} width="80" height="50" style={{position: "absolute", bottom: 0, right: 0}}/>
+						<a href="http://yelp.com">
+							<img src={require('../images/Yelp_trademark_RGB_outline.png')} width="80" height="50" style={{position: "absolute", bottom: 0, right: 0}}/>
 						</a>
 						<br />
-					</Col>		
+					</Col>
 				</div>
 				<Container>
 				{this.props.loggedIn && <Button onClick = {this.handleSubmit}>Favorite</Button>}
--- frontend/src/pages/RestaurantPage.js
+++ frontend/src/pages/RestaurantPage.js
@@ -1,29 +1,69 @@
 import React, { Component } from 'react';
 import { Row, Col, Card, CardImg,} from 'react-bootstrap';
-import searchObj from "../designPatterns/SearchStateSingleton"
-import TwoPointMapBox from "../components/TwoPointMapBox"
+import searchObj from "../designPatterns/SearchStateSingleton";
+import '../css/fontawesome/css/all.css';
+import StarRatingComponent from './StarRatingComponent.jsx';
 
 class RestaurantPage extends Component {
+    constructor (props) {
+      super();
+      this.state = {
+        rating_half_star: 0,
+      };
+      this.onStarClickHalfStar = this.onStarClickHalfStar.bind(this);
+    };
 
-  render() {
-	  console.log(searchObj.restaurants);
-    return (
-		<>	
-			<div className="restaurantPage">
-			<h2 className="restaurantTitle">{searchObj.restaurant.name}</h2>
-			<div className="restaurantInfo">
-			<img className="image" src={searchObj.restaurant.image_url} />
+    onStarClickHalfStar(nextValue, prevValue, name, e) {
+      const xPos = (e.pageX - e.currentTarget.getBoundingClientRect().left) / e.currentTarget.offsetWidth;
 
-			<a className="card-link" href={searchObj.restaurant.url}>Website</a>
-			<div className="card-text">location: {searchObj.restaurant.location.zip_code}</div>
-			<div className="card-text">Phone: {searchObj.restaurant.phone}</div>
-			<div className="card-text">Price: {searchObj.restaurant.price}</div>
-			<div className="card-text">Rating: {searchObj.restaurant.rating}</div>
-			</div>
-			</div>
-			<TwoPointMapBox />
-		</>
-	);
+      if (xPos <= 0.5) {
+        nextValue -= 0.5;
+      }
+
+      console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
+      //console.log(nextValue);
+      this.setState({rating_half_star: nextValue});
+    }
+
+    render() {
+      return (
+  		<div>
+  			<CardImg className="rounded" src={searchObj.restaurant.image_url} style={{display: "inline"}}/>
+  			<h2 style={{display: "inline", fontSize: '5em'}}>{searchObj.restaurant.name}</h2>
+  			<Card style={{fontSize: '1.5em', padding:"10px"}}>
+  				<a className="card-link" href={searchObj.restaurant.url}>Website</a>
+  				<div className="card-text">location: {searchObj.restaurant.location.zip_code}</div>
+  				<div className="card-text">Phone: {searchObj.restaurant.phone}</div>
+  				<div className="card-text">Price: {searchObj.restaurant.price}</div>
+  				<div className="card-text">Rating: {searchObj.restaurant.rating}</div>
+  			</Card>
+        <h3>Rate this Restaurant:</h3>
+        <div style={{fontSize: 24}}>
+          <StarRatingComponent
+            name="app6"
+            starColor="#ffb400"
+            emptyStarColor="#ffb400"
+            value={this.state.rating_half_star}
+            onStarClick={this.onStarClickHalfStar.bind(this)}
+            renderStarIcon={(index, value) => {
+              return (
+                <span>
+                  <i className={index <= value ? 'fas fa-star' : 'far fa-star'} />
+                </span>
+              );
+            }}
+            renderStarIconHalf={() => {
+              return (
+                <span>
+                  <span style={{position: 'absolute'}}><i className="far fa-star" /></span>
+                  <span><i className="fas fa-star-half" /></span>
+                </span>
+              );
+            }} />
+        </div>
+  		</div>
+  	);
   }
 }
+
 export default RestaurantPage;
